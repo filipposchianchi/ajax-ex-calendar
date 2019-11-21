06039acceabaec160b637ba21b7bcd6df512714a
filippo <schianchi.filippo.93@gmail.com>
2019-11-21 17:12:22 +0100

    milestone #2

--- javascript/script.js
+++ javascript/script.js
@@ -1,119 +1,79 @@
 $(document).ready( function() {
 
-    // var source   = document.getElementById("entry-template").innerHTML;
-    // var template = Handlebars.compile(source);
+    var url = "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month="
+    var mese = 0;
+    fAjax();
+    var mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"]
 
-    // var numeroDiGiorni = moment('01/01/2018', 'DD/MM/YYYY').daysInMonth();
-
-    // for(var i = 1; i <= numeroDiGiorni; i++) {
-
-    //     var currentDate = moment('2018-01-' + i, 'YYYY-MM-D').format('YYYY-MM-DD');
-
-
-    //     var currentDay = moment(currentDate).format('DD dddd');
-
-    //     var context = {data: currentDay};
-    //     var html    = template(context);
-
-    //     $(".container").append(html);
-    // }
-
-    $.ajax({
-        url : "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
-        method : "GET",
-        success : function (data) {
-            var festivita = data.response
-
-            var source   = document.getElementById("entry-template").innerHTML;
-            var template = Handlebars.compile(source);
-
-            var numeroDiGiorni = moment('01/01/2018', 'DD/MM/YYYY').daysInMonth();
-
-            for(var i = 1; i <= numeroDiGiorni; i++) {
-                var currentDate = moment('2018-01-' + i, 'YYYY-MM-D').format('YYYY-MM-DD');
-                var currentDay = moment(currentDate).format('DD dddd');
-
-                context = {data: currentDay};
-
-                var html = template(context);
-                $('.container').append('<div data-date="'+currentDate+'">'+ html +'</div>')
-            }
-
-            for (var i = 0; i < festivita.length; i++){
-                var oggetto = festivita[i];
-
-                var nomefesta = oggetto.name;
-                var datafesta = oggetto.date;
-
-
-                var elementoSelez = $("div[data-date='" + datafesta + "']");
-                console.log(elementoSelez);
-
-                if(elementoSelez){
-                      elementoSelez.addClass("red").append(" - " + nomefesta);
-                }
-            }
-
-
-
-            // for(var i = 1; i <= numeroDiGiorni; i++) {
-
-            //     var currentDate = moment('2018-01-' + i, 'YYYY-MM-D').format('YYYY-MM-DD');
-            //     var currentDay = moment(currentDate).format('DD dddd');
-               
-            //     var inserito=false
-
-            //     for (var j = 0; j< numeroGiorniFestivi; j++) {
-                    
-            //         var context;
-
-            //         if(currentDate == festivita[j].date && inserito == false ){
-            //             console.log(currentDate, festivita[j].date, j, "uguali")
-            //             context = {data: currentDay + " - " + festivita[j].name, colore: "red"};
-            //             inserito = true;
-
-            //         } else if (inserito ===false) {
-            //             console.log(currentDate, festivita[j].date, j, "diversi")
-            //             context = {data: currentDay, colore: "black"};
-            //         }
-
-            //         var html = template(context);
-            //         $(".container").append(html);
-                    
-
-            //     }
-                
-
-            //     // if(currentDate === festivita[0].date){
-            //     //     console.log(currentDate, "uguale", festivita[0].date)
-            //     //     var currentDay = moment(currentDate).format('DD dddd');
-            //     //     var context = {data: currentDay + " - " + festivita[0].name, colore: "red"};
-            //     //     var html = template(context);
-            //     //     $(".container").append(html);
-
-            //     // } else if (currentDate === festivita[1].date) {
-            //     //     console.log(currentDate, "uguale", festivita[1].date)
-            //     //     var currentDay = moment(currentDate).format('DD dddd');
-            //     //     var context = {data: currentDay + " - " + festivita[1].name, colore: "red"} ;
-            //     //     var html = template(context);
-            //     //     $(".container").append(html);
-
-            //     // } else {
-            //     //     var currentDay = moment(currentDate).format('DD dddd');
-            //     //     var context = {data: currentDay, colore: "black"};
-            //     //     var html = template(context);
-            //     //     $(".container").append(html);
-
-            //     // }
-
-
-            // }
-
-
-        },
-        error : function (errore) {
-            alert("E' avvenuto un errore. "+errore);
+    $(".successivo").click( function() {
+        if(mese==11) {
+            $(this).off();
+        } else {
+            $(".container").empty();
+            mese++;
+            fAjax();
         }
-    });
+    })
+    
+    $(".precedente").click( function() {
+        if(mese==0) {
+            $(this).off();
+        }else {
+            $(".container").empty();
+            mese--;
+            fAjax();
+        }
+    })
+
+
+    function fAjax() {
+        $.ajax({
+            url : url+mese,
+            method : "GET",
+            success : function (data) {
+                
+                var festivita = data.response
+
+                var source   = document.getElementById("entry-template").innerHTML;
+                var template = Handlebars.compile(source);
+
+                var numeroDiGiorni = moment('01/'+(mese+1)+'/2018', 'DD/MM/YYYY').daysInMonth();
+                console.log(numeroDiGiorni);
+
+                for(var i = 1; i <= numeroDiGiorni; i++) {
+                    var currentDate = moment('2018-' +(mese+1)+ '-' + i, 'YYYY-MM-D').format('YYYY-MM-DD');
+                    console.log(currentDate);
+                    var currentDay = moment(currentDate).format('DD dddd');
+                    console.log(currentDay);
+
+                    context = {data: currentDay};
+
+                    var html = template(context);
+                    $('.container').append('<div data-date="'+currentDate+'">'+ html +'</div>')
+                }
+
+                for (var i = 0; i < festivita.length; i++){
+                    var oggetto = festivita[i];
+
+                    var nomefesta = oggetto.name;
+                    var datafesta = oggetto.date;
+
+
+                    var elementoSelez = $("div[data-date='" + datafesta + "']");
+                    console.log(elementoSelez);
+
+                    if(elementoSelez){
+                        elementoSelez.addClass("red").append(" - " + nomefesta);
+                    }
+                }
+
+                $(".mese").text(mesi[mese])
+
+            },
+            error : function (errore) {
+                alert("E' avvenuto un errore. "+errore);
+            }
+        });
+    }
 
 })
